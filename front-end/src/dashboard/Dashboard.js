cfa043d02b4cb68c5f5f3ecfa00aeee18f329e50
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-29 09:24:29 -0400

    Modified attributes for date buttons and added a datepicker

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -46,25 +46,37 @@ function Dashboard({ date }) {
         <div>
           <button
             type="button"
-            className="btn btn-primary"
+            className="btn btn-info btn-sm mr-2"
             onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
           >
+            <span className="oi oi-chevron-left mr-2" />
             Previous Date
           </button>
           <button
             type="button"
-            className="btn btn-primary"
+            className="btn btn-info btn-sm mr-2"
             onClick={() => history.push(`/dashboard?date=${today()}`)}
           >
+            <span className="oi oi-calendar mr-2" />
             Today
           </button>
           <button
             type="button"
-            className="btn btn-primary"
+            className="btn btn-info btn-sm mr-2"
             onClick={() => history.push(`/dashboard?date=${next(date)}`)}
           >
             Next Date
+            <span className="oi oi-chevron-right ml-2" />
           </button>
+          <input
+            type="date"
+            className="form-control mt-2"
+            style={{ maxWidth: "150px" }}
+            onChange={(event) =>
+              history.push(`/dashboard?date=${event.target.value}`)
+            }
+            value={date}
+          />
         </div>
         <div>
           <ReservationsList reservations={reservations} />
