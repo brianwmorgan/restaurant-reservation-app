5e66e095e17fa64ec2a2819161aae71b0f33bb38
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-28 20:29:18 -0400

    modified ErrorAlert.js to resolve object/string error in Heroku frontend app

--- front-end/src/layout/ErrorAlert.js
+++ front-end/src/layout/ErrorAlert.js
@@ -9,7 +9,7 @@ import React from "react";
  */
 
 function ErrorAlert({ error }) {
-  return error && <div className="alert alert-danger m-2">Error: {error}</div>;
+  return error && <div className="alert alert-danger m-2">Error: {error.message || error}</div>;
 }
 
 export default ErrorAlert;
