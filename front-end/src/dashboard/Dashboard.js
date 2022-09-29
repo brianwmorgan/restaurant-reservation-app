74cfd712686c59c6675c9df6f46d3162e59b7a9d
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-29 13:05:37 -0400

    Bootstrap styling for buttons on dashboard

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -40,32 +40,33 @@ function Dashboard({ date }) {
     <main>
       <div>
         <h1>Dashboard</h1>
-
-        <div>
-          <button
-            type="button"
-            className="btn btn-info btn-sm mt-2 mb-2 mr-2"
-            onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
-          >
-            <span className="oi oi-chevron-left mr-2" />
-            Previous Date
-          </button>
-          <button
-            type="button"
-            className="btn btn-info btn-sm  mt-2 mb-2 mr-2"
-            onClick={() => history.push(`/dashboard?date=${today()}`)}
-          >
-            <span className="oi oi-calendar mr-2" />
-            Today
-          </button>
-          <button
-            type="button"
-            className="btn btn-info btn-sm mt-2 mb-2 mr-2"
-            onClick={() => history.push(`/dashboard?date=${next(date)}`)}
-          >
-            Next Date
-            <span className="oi oi-chevron-right ml-2" />
-          </button>
+        <div className="input-group input-group-sm mb-2">
+          <div className="d-flex d-md-inline btn-group input-group-prepend">
+            <button
+              type="button"
+              className="btn btn-info btn-sm mt-2 mb-2 mr-2"
+              onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
+            >
+              <span className="oi oi-chevron-left mr-2" />
+              Previous Date
+            </button>
+            <button
+              type="button"
+              className="btn btn-info btn-sm  mt-2 mb-2 mr-2"
+              onClick={() => history.push(`/dashboard?date=${today()}`)}
+            >
+              <span className="oi oi-calendar mr-2" />
+              Today
+            </button>
+            <button
+              type="button"
+              className="btn btn-info btn-sm mt-2 mb-2 mr-2"
+              onClick={() => history.push(`/dashboard?date=${next(date)}`)}
+            >
+              Next Date
+              <span className="oi oi-chevron-right ml-2" />
+            </button>
+          </div>
           <input
             type="date"
             className="form-control mt-2 mb-2"
