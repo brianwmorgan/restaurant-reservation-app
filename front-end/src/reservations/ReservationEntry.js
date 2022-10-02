380770af2547a51507138ddcd9c0d695fb047563
brianwmorgan <brianwmorgan317@gmail.com>
2022-10-02 11:31:16 -0400

    Changed reservations list from a table to Bootstrap cards

--- front-end/src/reservations/ReservationEntry.js
+++ front-end/src/reservations/ReservationEntry.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { getDisplayTime } from "../utils/date-time";
+import { getDisplayDate, getDisplayTime } from "../utils/date-time";
 
 export default function ReservationEntry({ reservation }) {
   const URL = process.env.REACT_APP_API_BASE_URL;
@@ -25,19 +25,91 @@ export default function ReservationEntry({ reservation }) {
     }
   };
 
+  const displayDate = getDisplayDate(reservation.reservation_date);
   const displayTime = getDisplayTime(reservation.reservation_time);
 
+  // return (
+  //   <tr>
+  //     <td className="text-left align-middle">{reservation.first_name}</td>
+  //     <td className="text-left align-middle">{reservation.last_name}</td>
+  //     <td className="text-center align-middle">{reservation.mobile_number}</td>
+  //     <td className="text-center align-middle">{displayTime}</td>
+  //     <td className="text-center align-middle">{reservation.people}</td>
+  //     <td className="text-center align-middle" data-reservation-id-status={reservation.reservation_id}>
+  //       {reservation.status}
+  //     </td>
+  //     <td className="text-center align-middle">
+  //       {reservation.status === "booked" && (
+  //         <Link to={`/reservations/${reservation.reservation_id}/seat`}>
+  //           <button className="btn btn-sm btn-primary">
+  //             <span className="oi oi-people mr-2" />
+  //             Seat
+  //           </button>
+  //         </Link>
+  //       )}
+  //     </td>
+  //     <td className="text-center align-middle">
+  //       {reservation.status === "booked" && (
+  //         <Link to={`/reservations/${reservation.reservation_id}/edit`}>
+  //           <button className="btn btn-sm btn-secondary">
+  //             <span className="oi oi-pencil mr-2" />
+  //             Edit
+  //           </button>
+  //         </Link>
+  //       )}
+  //     </td>
+  //     <td className="text-center align-middle">
+  //       {reservation.status === "booked" && (
+  //         <button
+  //           className="btn btn-sm btn-danger"
+  //           data-reservation-id-cancel={reservation.reservation_id}
+  //           onClick={handleCancelClick}
+  //         >
+  //           <span className="oi oi-trash mr-2" />
+  //           Cancel
+  //         </button>
+  //       )}
+  //     </td>
+  //   </tr>
+  // );
+
+  const statusColor = {
+    booked: "success",
+    seated: "primary",
+    finished: "secondary",
+    cancelled: "danger",
+  };
+
   return (
-    <tr>
-      <td className="text-left align-middle">{reservation.first_name}</td>
-      <td className="text-left align-middle">{reservation.last_name}</td>
-      <td className="text-center align-middle">{reservation.mobile_number}</td>
-      <td className="text-center align-middle">{displayTime}</td>
-      <td className="text-center align-middle">{reservation.people}</td>
-      <td className="text-center align-middle" data-reservation-id-status={reservation.reservation_id}>
-        {reservation.status}
-      </td>
-      <td className="text-center align-middle">
+    <div className="row flex-column flex-md-row bg-light border mx-1 my-3 px-2 py-2">
+      <div
+        className={`col text-center text-md-left align-self-center mr-3`}
+        style={{ maxWidth: "100px" }}
+      >
+        <span
+          className={`my-2 badge text-white bg-${
+            statusColor[reservation.status]
+          }`}
+          data-reservation-id-status={reservation.reservation_id}
+        >
+          {reservation.status}
+        </span>
+      </div>
+      <div className="col align-self-center">
+        <h5 className="mb-1">
+          {reservation.first_name} {reservation.last_name}
+        </h5>
+        <p className="mb-0">
+          Party of {reservation.people}
+          <span className="ml-3">{reservation.mobile_number}</span>
+        </p>
+      </div>
+      <div className={`col align-self-center`}>
+        <p className="mb-0">{displayDate.display}</p>
+        <p className="mb-0">{displayTime}</p>
+      </div>
+
+      <div className="col align-self-center">
         {reservation.status === "booked" && (
           <Link to={`/reservations/${reservation.reservation_id}/seat`}>
             <button className="btn btn-sm btn-primary">
@@ -46,8 +118,7 @@ export default function ReservationEntry({ reservation }) {
             </button>
           </Link>
         )}
-      </td>
-      <td className="text-center align-middle">
+
         {reservation.status === "booked" && (
           <Link to={`/reservations/${reservation.reservation_id}/edit`}>
             <button className="btn btn-sm btn-secondary">
@@ -56,8 +127,7 @@ export default function ReservationEntry({ reservation }) {
             </button>
           </Link>
         )}
-      </td>
-      <td className="text-center align-middle">
+
         {reservation.status === "booked" && (
           <button
             className="btn btn-sm btn-danger"
@@ -68,7 +138,7 @@ export default function ReservationEntry({ reservation }) {
             Cancel
           </button>
         )}
-      </td>
-    </tr>
+      </div>
+    </div>
   );
 }
--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -11,26 +11,31 @@ export default function ReservationsList({ reservations, searchMode }) {
         />
       );
     });
+    // return (
+    //   <div>
+    //     <table className="table table-success table-striped table-hover table-bordered table-responsive-sm">
+    //       <thead className="table table-success-dark">
+    //         <tr>
+    //           <th scope="col" className="text-left">First Name</th>
+    //           <th scope="col" className="text-left">Last Name</th>
+    //           <th scope="col" className="text-center">Mobile Number</th>
+    //           <th scope="col" className="text-center">Time</th>
+    //           <th scope="col" className="text-center"># of People</th>
+    //           <th scope="col" className="text-center">Status</th>
+    //           <th scope="col" className="text-center">Seat</th>
+    //           <th scope="col" className="text-center">Edit</th>
+    //           <th scope="col" className="text-center">Cancel</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>{reservationsList}</tbody>
+    //     </table>
+    //   </div>
+    // );
+
     return (
-      <div>
-        <table className="table table-success table-striped table-hover table-bordered table-responsive-sm">
-          <thead className="table table-success-dark">
-            <tr>
-              <th scope="col" className="text-left">First Name</th>
-              <th scope="col" className="text-left">Last Name</th>
-              <th scope="col" className="text-center">Mobile Number</th>
-              <th scope="col" className="text-center">Time</th>
-              <th scope="col" className="text-center"># of People</th>
-              <th scope="col" className="text-center">Status</th>
-              <th scope="col" className="text-center">Seat</th>
-              <th scope="col" className="text-center">Edit</th>
-              <th scope="col" className="text-center">Cancel</th>
-            </tr>
-          </thead>
-          <tbody>{reservationsList}</tbody>
-        </table>
-      </div>
-    );
+      <div>{reservationsList}</div>
+    )
+
   } else {
     const reservationsList = reservations
       .filter((reservation) => {
@@ -45,25 +50,29 @@ export default function ReservationsList({ reservations, searchMode }) {
         );
       });
 
+    // return (
+    //   <div>
+    //     <table className="table table-success table-striped table-hover table-bordered table-responsive-sm">
+    //       <thead className="table table-success-dark">
+    //         <tr>
+    //           <th scope="col" className="text-left">First Name</th>
+    //           <th scope="col" className="text-left">Last Name</th>
+    //           <th scope="col" className="text-center">Mobile Number</th>
+    //           <th scope="col" className="text-center">Time</th>
+    //           <th scope="col" className="text-center"># of People</th>
+    //           <th scope="col" className="text-center">Status</th>
+    //           <th scope="col" className="text-center">Seat</th>
+    //           <th scope="col" className="text-center">Edit</th>
+    //           <th scope="col" className="text-center">Cancel</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>{reservationsList}</tbody>
+    //     </table>
+    //   </div>
+    // );
+
     return (
-      <div>
-        <table className="table table-success table-striped table-hover table-bordered table-responsive-sm">
-          <thead className="table table-success-dark">
-            <tr>
-              <th scope="col" className="text-left">First Name</th>
-              <th scope="col" className="text-left">Last Name</th>
-              <th scope="col" className="text-center">Mobile Number</th>
-              <th scope="col" className="text-center">Time</th>
-              <th scope="col" className="text-center"># of People</th>
-              <th scope="col" className="text-center">Status</th>
-              <th scope="col" className="text-center">Seat</th>
-              <th scope="col" className="text-center">Edit</th>
-              <th scope="col" className="text-center">Cancel</th>
-            </tr>
-          </thead>
-          <tbody>{reservationsList}</tbody>
-        </table>
-      </div>
-    );
+      <div>{reservationsList}</div>
+    )
   }
 }
